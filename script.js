c22c92da71afcbe8f7a605c7dbff88fd238383dd
Seth Johnston <ruling.ants.0d@icloud.com>
2023-12-08 17:06:31 +0900

    Changes to be committed: 	modified:   index.html 	modified:   script.js

--- script.js
+++ script.js
@@ -1,31 +1,6 @@
 // Check if there is existing data in localStorage
 let guestHouses = JSON.parse(localStorage.getItem('guestHouses')) || [];
 
-// Function to save guest house data
-function saveGuestHouse() {
-    const guestHouse = {
-        name: document.getElementById('name').value,
-        location: document.getElementById('location').value,
-        costPerNight: parseFloat(document.getElementById('costPerNight').value),
-        acCostPerNight: parseFloat(document.getElementById('acCostPerNight').value),
-        truckCostPerKm: parseFloat(document.getElementById('truckCostPerKm').value),
-        carCostPerKm: parseFloat(document.getElementById('carCostPerKm').value),
-        motorbikeCostPerKm: parseFloat(document.getElementById('motorbikeCostPerKm').value),
-    };
-
-    // Add the new guest house to the array
-    guestHouses.push(guestHouse);
-
-    // Save the array to localStorage
-    localStorage.setItem('guestHouses', JSON.stringify(guestHouses));
-
-    // Update the guest houses list
-    updateGuestHousesList();
-
-    // Clear the form
-    document.getElementById('guestHouseForm').reset();
-}
-
 // Function to update the guest houses list
 function updateGuestHousesList() {
     const guestHousesList = document.getElementById('guestHousesList');
@@ -41,11 +16,46 @@ function updateGuestHousesList() {
             <p><strong>Truck Cost per Km:</strong> ${guestHouse.truckCostPerKm}</p>
             <p><strong>Car Cost per Km:</strong> ${guestHouse.carCostPerKm}</p>
             <p><strong>Motorbike Cost per Km:</strong> ${guestHouse.motorbikeCostPerKm}</p>
+            <p><strong>Last Edit:</strong> ${guestHouse.lastEdit}</p>
             <button onclick="editGuestHouse(${index})">Edit</button>
             <button onclick="deleteGuestHouse(${index})">Delete</button>
         `;
         guestHousesList.appendChild(listItem);
     });
+}
+
+// Function to save guest house data
+function saveGuestHouse() {
+    const now = new Date();
+    const guestHouse = {
+        name: document.getElementById('name').value,
+        location: document.getElementById('location').value,
+        costPerNight: parseFloat(document.getElementById('costPerNight').value),
+        acCostPerNight: parseFloat(document.getElementById('acCostPerNight').value),
+        truckCostPerKm: parseFloat(document.getElementById('truckCostPerKm').value),
+        carCostPerKm: parseFloat(document.getElementById('carCostPerKm').value),
+        motorbikeCostPerKm: parseFloat(document.getElementById('motorbikeCostPerKm').value),
+        lastEdit: now.toLocaleString(), // Store the date and time of the last edit
+    };
+
+    // Check if editing an existing guest house
+    const editIndex = document.getElementById('editIndex').value;
+    if (editIndex !== '') {
+        guestHouses[editIndex] = guestHouse;
+    } else {
+        // Add the new guest house to the array
+        guestHouses.push(guestHouse);
+    }
+
+    // Save the array to localStorage
+    localStorage.setItem('guestHouses', JSON.stringify(guestHouses));
+
+    // Update the guest houses list
+    updateGuestHousesList();
+
+    // Clear the form and editIndex value
+    document.getElementById('guestHouseForm').reset();
+    document.getElementById('editIndex').value = '';
 }
 
 // Function to delete a guest house
@@ -68,6 +78,6 @@ function editGuestHouse(index) {
     document.getElementById('carCostPerKm').value = guestHouse.carCostPerKm;
     document.getElementById('motorbikeCostPerKm').value = guestHouse.motorbikeCostPerKm;
 
-    // Delete the guest house from the array
-    deleteGuestHouse(index);
+    // Set the editIndex value to the index of the guest house
+    document.getElementById('editIndex').value = index;
 }
